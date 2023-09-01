3a20e75ffe1e69e293d2456fef7977e3d6ea5c30
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2023-08-31 22:02:51 -0300

    feat: train de bot for facebook and twitter

--- server/api/ai.post.ts
+++ server/api/ai.post.ts
@@ -1,16 +1,24 @@
 
 import OpenAI from "openai";
-import { customerSupportAgent } from '~/agents'
+// import { customerSupportAgent } from '~/agents'
+import * as agents from '~/agents'
 
 interface EventBody {
   messages: OpenAI.Chat.ChatCompletionMessage[]
   temperature?: number | null
+  agent: string | null,
+  url?: string | null
 }
 
 export default defineEventHandler(async (event) => {
   const { OPENAI_API_KEY } = useRuntimeConfig()
 
   const body = await readBody<EventBody>(event)
+  const agent = body.agent || 'customerSupportAgent'
+
+  if (!Object.keys(agents).includes(agent)) {
+    throw new Error(`Agent ${agent} not found`)
+  }
 
   const openai = new OpenAI({
     apiKey: OPENAI_API_KEY
@@ -20,7 +28,10 @@ export default defineEventHandler(async (event) => {
     model: "gpt-3.5-turbo",
     messages: body.messages || [],
     temperature: body.temperature || 1,
-    ...customerSupportAgent(body)
+    // ...customerSupportAgent(body) // example only agent
+
+    //@ts-ignore checking abouve if agent exists
+    ...agents[agent](body)
   });
 
   return completion
