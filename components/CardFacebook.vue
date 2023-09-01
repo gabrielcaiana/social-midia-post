aa0fc9dae216f72a3f93b9d6187140e6ee903162
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2023-08-31 23:43:28 -0300

    feat: make posting a click way

--- components/CardFacebook.vue
+++ components/CardFacebook.vue
@@ -9,10 +9,23 @@ const announcement = computed(() => firstMessage.value?.content ?? '')
 
 const generate = () => nextTick(() => chat(props))
 defineExpose({ generate })
+
+const postURL = computed(
+  () => `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(props.url)}`
+)
+
+const { copy } = useClipboard()
+
+const post = () => {
+  copy(announcement.value)
+  setTimeout(() => window.open(postURL.value, '_blank'), 500)
+}
 </script>
 <template>
   <CardGeneric title="Facebook" :state="state" :body="announcement">
     <button class="btn btn-neutral" @click="generate">Regenerate</button>
-    <a class="btn btn-primary"> Post </a>
+    <a :href="postURL" class="btn btn-primary" target="_blank" @click.prevent="post">
+      Copy Announcement and Post
+    </a>
   </CardGeneric>
 </template>
--- components/CardTwitter.vue
+++ components/CardTwitter.vue
@@ -9,6 +9,13 @@ const announcement = computed(() => firstMessage.value?.content ?? '')
 
 const generate = () => nextTick(() => chat(props))
 defineExpose({ generate })
+
+const postURL = computed(
+  () =>
+    `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+      announcement.value || ''
+    )}`
+)
 </script>
 <template>
   <CardGeneric title="Twitter" :state="state" :body="announcement" class="mb-10">
@@ -19,7 +26,7 @@ defineExpose({ generate })
       </div>
       <div>
         <button class="btn btn-neutral" @click="generate">Regenerate</button>
-        <a class="btn btn-primary" target="_blank">Post</a>
+        <a :href="postURL" class="btn btn-primary" target="_blank">Post</a>
       </div>
     </div>
   </CardGeneric>
